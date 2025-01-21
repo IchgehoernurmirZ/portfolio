16d6621005d7c4cb074f1f54bc9e7ff262bfe3e0
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2025-01-21 17:20:54 +0800

    Add animation for about me section

--- frontend/components/about.tsx
+++ frontend/components/about.tsx
@@ -6,7 +6,13 @@ import SectionHeading from './section-heading';
 
 export default function About() {
   return (
-    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+    initial={{opacity: 0, y: 100}}
+    animate={{opacity: 1, y: 0}}
+    transition={{
+        delay: 0.175,
+    }}
+    >
         <SectionHeading>About me</SectionHeading>
         <p className='mb-3'>
         After graduating with a degree in{" "}
