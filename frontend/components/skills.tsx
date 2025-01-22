24d3c0c4d4313b3f42e2b0870c44121776f3a6be
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2025-01-22 15:49:53 +0800

    Animate the stagger effect to skills section

--- frontend/components/skills.tsx
+++ frontend/components/skills.tsx
@@ -4,6 +4,16 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "../lib/data";
 import { useSectionInView } from "../lib/hooks";
+import { animate, motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: { opacity: 0, y: 100 },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: 0.05 * index },
+  }),
+};
 
 export default function Skills() {
   const { ref } = useSectionInView("Skills");
@@ -16,12 +26,17 @@ export default function Skills() {
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li
+          <motion.li
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            custom={index}
             key={index}
             className="bg-white border border-black/[0.1] rounded-xl px-5 py-3"
           >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
