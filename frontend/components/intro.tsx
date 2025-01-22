b2a7366f62d629831dff1ab88227de826bdc0d2e
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2025-01-22 18:09:05 +0800

    Add resume for downloading

--- frontend/components/intro.tsx
+++ frontend/components/intro.tsx
@@ -91,7 +91,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
-          href=""
+          href="./resume.pdf"
           download
         >
           Download Resume{" "}
