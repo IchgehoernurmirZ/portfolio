ea50002f4ddd77508f24cef80dac651120b9a56b
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2025-01-23 13:17:23 +0800

    Refactor submit btn and add animation

--- frontend/components/contact.tsx
+++ frontend/components/contact.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "../lib/hooks";
 import { sendEmail } from "../actions/sendEmail";
+import SubmitBtn from "./submit-btn";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -48,13 +48,7 @@ export default function Contact() {
           maxLength={5000}
           name="message"
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
-        >
-          Submit
-          <FaPaperPlane className="group-hover:translate-x-1 group-hover:-translate-y-1 text-xs opacity-70 transition-all" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
---
+++ frontend/components/submit-btn.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useFormStatus } from "react-dom";
+import { FaPaperPlane } from "react-icons/fa";
+
+export default function SubmitBtn() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 disabled:scale-100 disabled:bg-opacity-65 hover:bg-gray-950 active:scale-105"
+    >
+      {pending ? (
+        <div className="h-5 w-5 animate-spin rounde-full border-b-2 border-white"></div>
+      ) : (
+        <>
+          Submit
+          <FaPaperPlane className="group-hover:translate-x-1 group-hover:-translate-y-1 text-xs opacity-70 transition-all" />{" "}
+        </>
+      )}
+    </button>
+  );
+}
