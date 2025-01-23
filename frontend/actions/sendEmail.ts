ab085c70a72e9341be575ddfe0dae3b874212756
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2025-01-23 12:08:23 +0800

    Style the email being send through the form using react email

--- frontend/actions/sendEmail.ts
+++ frontend/actions/sendEmail.ts
@@ -2,6 +2,8 @@
 
 import { Resend } from "resend";
 import { getErrorMessage, validateString } from "../lib/utils";
+import ContactFormEmail from "../email/contact-form-email";
+import React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -18,7 +20,10 @@ export const sendEmail = async (formData: FormData) => {
       to: "zoey42zhang@gmail.com",
       subject: "Message from contact form",
       replyTo: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error: unknown) {
     return {
