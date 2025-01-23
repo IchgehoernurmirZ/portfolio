68ff8c523181ad32559050a0833b8995e3a5005d
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2025-01-23 16:00:48 +0800

    Fix styling issue on smaller screen

--- frontend/components/project.tsx
+++ frontend/components/project.tsx
@@ -26,8 +26,8 @@ export default function Project({
       style={{ scale: scaleProgress, opacity: opacityProgress }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="group bg-gray-100 rounded-lg max-w-[42rem] border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8 dark:bg-white/10 dark:hover:bg-whit/10 dark:text-white">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      <section className="group bg-gray-100 rounded-lg max-w-[42rem] border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-whit/10 dark:text-white">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -47,9 +47,9 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] 
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] 
         group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2
-        transition "
+        transition"
         />
       </section>
     </motion.div>
